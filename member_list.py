9f6dad26334e0b669612b8cf564619f2748cd896
tomoyk <tmyk.kym@gmail.com>
2021-02-14 15:04:17 +0900

    add member-list

---
+++ member_list.py
@@ -0,0 +1,48 @@
+import json
+import os
+import urllib.request
+
+
+SLACK_TOKEN = os.environ.get("SLACK_BOT_TOKEN")
+SLACK_WEBHOOK = f"https://slack.com/api/users.list?token={SLACK_TOKEN}&pretty=1"
+EXCLUDE_USERS = {"slackbot"}
+
+
+def get_members():
+    assert len(SLACK_TOKEN) > 10, "Invalid Slack Token"
+    response = urllib.request.urlopen(SLACK_WEBHOOK)
+    content = json.loads(response.read().decode('utf8'))
+
+    members_dict = {}
+    for member in content['members']:
+        # api doc) https://api.slack.com/methods/users.list
+        if member['is_bot'] or member['deleted'] or member['name'] in EXCLUDE_USERS:
+            continue
+        # print(member)
+        uid = str(member['id'])
+        name = str(member['name'])  # add oauth permission 'users:read.email'
+        email = str(member['profile'].get('email'))
+        if email is None:
+            continue
+        student_id = str(email[0:8]).upper()
+        real_name = str(member.get('real_name'))
+        # print('{0:<10}\t{1:<10}\t{2:<10}'.format(uid, name, real_name))
+        members_dict[student_id] = {
+            "email": email,
+            "name": name,
+            "uid": uid,
+            "real_name": real_name
+        }
+
+    return members_dict
+
+
+def main():
+    _members_dict = get_members()
+    with open('member_list.json', mode='w') as f:
+        json.dump(_members_dict, f, ensure_ascii=False, indent=4,
+                  sort_keys=False, separators=(',', ': '))
+
+
+if __name__ == '__main__':
+    main()
